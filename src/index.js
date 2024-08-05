d0776b7ddf9285bd0c51ef6c7fb85abe199745c3
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-06 00:26:03 +0500

    "Set up initial React project and components
    - Added introductory comments about React.
    - Set up the development environment with initial configurations.
    - Implemented basic React functions and examples, including Greeting component variations.
    - Created BookList component to display a list of books with individual Book, Image, Title, and Author components.
    - Updated CSS class names and styles to ensure proper layout and responsiveness."

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Note the updated import
+import './index.css'
 
+//    (1)
+// function Greeting() {
+//     return (
+//         <div>
+//             <h1>No Hello, World!</h1>
+//         </div>
+//     );
+// }
+
+
+//    (2)
+// function Greeting() {
+//   return React.createElement('h1',{},'hello world')
+// }
+
+ 
+
+
+//    (3)
+// function Greeting() {
+//   return (
+//     <div>
+//       <  Person /> 
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// } 
+// const Person=() =><h2>Muhammad Zakriya</h2>
+// 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Greeting />);
+
+
+//    (4)
+// function Greeting() {
+//   return (
+//     <div>
+//       <  Person /> 
+//       <Message />
+//     </div>
+//   );
+// } 
+// const Person=() =><h2>Muhammad Zakriya</h2>
+
+// const Message=() =><h1>Hello, World!</h1>
+
+//     (5)
+
+
+function BookList() {
+  return (
+   <section className='BookList'>
+    <h1>My Book List</h1>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  
+   </section>
+  );
+} 
+
+const Book = () => {
+  return (
+   <article className='book'>
+    <Image></Image>
+    <Title></Title>
+    <Author></Author>
+   </article>
+  );
+}
+
+const Image =()=>{
+  return (
+   <img src="https://images-na.ssl-images-amazon.com/images/I/9101MLPcFTL._AC_UL254_SR254,254_.jpg" alt="Book Cover"/>
+  );
+}
+const Title =()=>{
+  return (
+   <h3>Title: The Great Gatsby</h3>
+  );
+}
+
+const Author =()=>{
+  return (
+   <h4>Author: Muhammad Zakriya</h4>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
